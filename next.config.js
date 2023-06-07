64aba25617505f68152e870ec9969e4ef1e4c024
Antun Karlovac <antun@antunkarlovac.com>
2023-06-07 10:12:48 -0700

    Topic #1 / Update next.config.json to use current working URL

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
     target: 'serverless',
-    assetPrefix: isProd ? 'https://asia-east2-nextjs-serverless.cloudfunctions.net/nextjs' : '',
+    assetPrefix: isProd ? 'https://us-central1-react-test-389022.cloudfunctions.net/nextjs' : '',
 }
